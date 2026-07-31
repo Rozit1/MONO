aeabb461ae0b5cdf0ef4c48625d52158bbc37b11
Rozit1 <chaulagainrojit41@gmail.com>
2026-07-31 09:11:56 +0545

    made onboarding screen

---
+++ app/index.tsx
@@ -0,0 +1,65 @@
+import { Pressable, Text, View, StyleSheet} from "react-native";
+import { Image } from "expo-image";
+import Background from "@/assets/svg/onboardingbg.svg";
+import { Button } from "@/components/buttons";
+import { router } from "expo-router";
+
+const styles = StyleSheet.create({
+  
+  logo: {
+    position: "absolute",
+    top: 100,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }, 
+
+  bottomContainer: {
+    paddingHorizontal: 28,
+    marginBottom: 40,
+    width: "100%",
+    marginTop: "auto", 
+  },
+
+  footer: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 18,
+    color: "#444444",
+  },
+
+  login: {
+    color: "#4D9D8F",
+    fontWeight: "600",
+  },
+
+})
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View>
+          <Background />
+          <Image
+              source={require("@/assets/svg/onboarding.png")}
+              style={styles.logo}
+          />
+      </View>
+     
+
+       <View style={styles.bottomContainer}>
+        <Button
+          title="Get Started"
+          type="primary"
+          // onPress={() => router.push("/profile")}
+        />
+
+        <Text style={styles.footer}>
+          Already Have Account?
+          <Text style={styles.login}> Log In</Text>
+        </Text>
+      </View>
+
+    </View>
+  );
+}
---
+++ components/buttons.tsx
@@ -0,0 +1,50 @@
+import { Pressable, PressableProps, StyleSheet, Text, ViewStyle } from "react-native"
+
+type Props = PressableProps & {
+    title: string
+    type: 'primary' | 'secondary' | 'outline' | 'danger' | 'disabled'
+}
+
+const buttonStyles: Record<Props['type'], ViewStyle> = {
+    primary: {
+        backgroundColor: '#3F8782',
+    },
+    secondary: {
+        backgroundColor: 'green',
+    },
+    outline: {
+        borderWidth: 1,
+        borderColor: 'blue',
+    },
+    danger: {
+        backgroundColor: 'red',
+    },
+    disabled: {
+        backgroundColor: 'gray',
+    },
+} as const
+
+const styles = StyleSheet.create({
+    common: {
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        borderRadius: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 5,
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+})
+
+export function Button({ title, type = 'primary', ...props }: Props) {
+    
+    return (
+        <Pressable style={[buttonStyles[type], styles.common ]} {...props}>
+            <Text style={styles.text}>{title}</Text>
+        </Pressable>
+    )
+}
